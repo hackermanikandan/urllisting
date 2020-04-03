a915f22a088935ea4698d0eb28a8cf2d7f06fd53
hackermanikandan <35624034+hackermanikandan@users.noreply.github.com>
2020-04-03 09:44:02 +0530

    Update urllister.py

--- urllister.py
+++ urllister.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import socket
 
 
 banner = """
@@ -11,7 +12,9 @@ banner = """
                                    
                           by mr.prohack
 """
-using = "urllister.py [dir or dns] [domain name] [wordlist]\nex: urllister.py dir google.com wordlist.txt"
+using = "urllister.py [dir or dns or dnsip] [domain name] [wordlist]\nex: urllister.py dir google.com wordlist.txt"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if len(sys.argv) <= 1:   
     print(banner)
@@ -20,11 +23,19 @@ if len(sys.argv) <= 1:
 
 def request(domain_name,out_file):
     try:
-        #host_ip = socket.gethostbyname(domain_name)
         req = requests.get(domain_name,timeout=2)
         if req.ok:
-            print('[+] 200 :'+req.url)
-            out_file.write(req.url+'\n')
+            if sys.argv[1] == 'dnsip':
+                url1 = ((req.url).find("//"))
+                url1 = (req.url[url1+2:])
+                url2 = url1.find('/')
+                domain = (url1[:url2])
+                host_ip = socket.gethostbyname(domain)
+                print('[+] 200 :'+req.url+'  ip :'+host_ip)
+                out_file.write(req.url+'       ip :'+host_ip+'\n')
+            else:
+                print('[+] 200 :'+req.url)
+                out_file.write(req.url+'\n')
         else:
             print('[-] 404 :'+domain_name)
     except KeyboardInterrupt:
@@ -41,31 +52,43 @@ def url(wordlist,domain,out_file):
                 one_line = one_line.strip()
                 domain_name = ('http://{}/{}'.format(domain,one_line))
                 request(domain_name,out_file)
+
 def domainlist(wordlist,domain,out_file):
     for one_line in wordlist:
                 one_line = one_line.strip()
                 domain_name = ('http://{}.{}'.format(one_line,domain))
                 request(domain_name,out_file)
-                print(new)
+
+def dnsip(wordlist,domain,out_file):
+    domainlist(wordlist,domain,out_file)
+
 
 def main():
     print(banner)
-    domain = sys.argv[2]  
+    try:
+        domain = sys.argv[2]  
+    except IndexError:
+        print('input files mising')
     out_file = open('live_urls.txt','w')
     try:
         if len(sys.argv[3]) != None:
             word = sys.argv[3]
     except:
         word = 'wordlist.txt'
-        print('use default wordlist\n')
-    wordlist = open(word,'r+')
-    if sys.argv[1] == 'dir':
-        url(wordlist,domain,out_file)
-    elif sys.argv[1] == 'dns':
-        domainlist(wordlist,domain,out_file)
-    else:
-        print(using)
-
+    try: 
+        if sys.argv[2] == None:   
+            print('use default wordlist\n')
+        wordlist = open(word,'r+')
+        if sys.argv[1] == 'dir':
+            url(wordlist,domain,out_file)
+        elif sys.argv[1] == 'dns':
+            domainlist(wordlist,domain,out_file)
+        elif sys.argv[1] == 'dnsip':
+            dnsip(wordlist,domain,out_file)
+        else:
+            print(using)
+    except:
+        exit()
 
 if __name__ == "__main__":
     main()
